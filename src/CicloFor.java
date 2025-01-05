b88341521b56823fa60c3ad9ad0caa76800989da
Diallo2024 <urymamadu@gmail.com>
2025-01-05 01:47:03 +0100

    Ciclos en Java

---
+++ src/CicloFor.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class CicloFor {
+    public static void main(String[] args) {
+        System.out.println("*** Ciclo for ***");
+
+        for (var contador = 0; contador <= 3; contador++){
+            System.out.println(contador);
+        }
+    }
+}
---
+++ src/NumerosPares.java
@@ -0,0 +1,13 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        System.out.println("*** Numeros pares ***");
+
+        var contador = 0;
+        while (contador <= 20) {
+            if (contador % 2 == 0)
+                System.out.print(contador + " ");
+                contador++;
+            }
+        }
+    }
+
