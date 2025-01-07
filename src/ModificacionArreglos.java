be53059e19b9ab041dfa9dd541f6a2dcbf961fd8
Diallo2024 <urymamadu@gmail.com>
2025-01-07 01:09:06 +0100

    Modificacion de arreglos

---
+++ src/ModificacionArreglos.java
@@ -0,0 +1,11 @@
+public class ModificacionArreglos {
+    public static void main(String[] args) {
+        // Arreglos
+        var enteros = new int[5];
+        // Modificar los elementos
+        enteros[0] = 5;
+        enteros[1] = 7;
+        enteros[3]= 4;
+
+    }
+}
