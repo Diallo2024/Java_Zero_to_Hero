b9e056a344fceafeb2f51754809d7324c9875033
Diallo2024 <urymamadu@gmail.com>
2025-01-07 22:50:22 +0100

    Cadenas

---
+++ src/CaracteresEspeciales.java
@@ -0,0 +1,21 @@
+public class CaracteresEspeciales {
+    public static void main(String[] args) {
+        //Caracteres especiales
+        //\t tabulación
+        var cadena1 = "Hola\nMundo";
+        System.out.println("cadena1 = " + cadena1);
+
+        //\' agregar une comilla simple
+        var cadena2 = "Hola \' Mundo";
+        System.out.println("cadena2 = " + cadena2);
+
+        //\" agregar una comilla double
+        var cadena3 = "Hola \" Mundo";
+        System.out.println("cadena3 = " + cadena3);
+
+        //agragar una barra invertida
+        var cadena5 = "Hola \\ Munod";
+        System.out.println("cadena5 = " + cadena5);
+
+    }
+}
