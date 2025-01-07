81fa54da31377460c031480b4f0d620a6fa425ef
Diallo2024 <urymamadu@gmail.com>
2025-01-07 01:36:16 +0100

    Modificacion de arreglos

--- src/ModificacionArreglos.java
+++ src/ModificacionArreglos.java
@@ -6,6 +6,9 @@ public class ModificacionArreglos {
         enteros[0] = 5;
         enteros[1] = 7;
         enteros[3]= 4;
-
+        // Lectura de valores de un arreglo
+        System.out.println("El valor del índice 0 es: " + enteros[0]);
+        System.out.println("El valor del ínidce 1 es: " + enteros[1]);
+        System.out.println("El valor del índice 3 es: " + enteros[3]);
     }
 }
