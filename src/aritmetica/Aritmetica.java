05b25e61d396d73b17a81e9caf7a0b3f0230540a
Diallo2024 <urymamadu@gmail.com>
2025-01-24 00:15:59 +0100

    update: encapsulamiento de objetos en java

--- src/aritmetica/Aritmetica.java
+++ src/aritmetica/Aritmetica.java
@@ -1,8 +1,8 @@
 package aritmetica;
 
 public class Aritmetica {
-    int operando1;
-    int operando2;
+    private int operando1;
+    private int operando2;
 
     // Constructor vacio
     public Aritmetica(){
@@ -24,4 +24,25 @@ public class Aritmetica {
         var resrultado = operando1 - operando2;
         System.out.println("Resrultado: " + resrultado);
     }
+
+    // Definimos el método Get del atributo 1
+    public int getOperando1(){
+        return this.operando1;
+    }
+
+    // Definimos el método Set del atributo 1
+    public void setOperando1(int operando1){
+        this.operando1 = operando1;
+    }
+
+    // Definimos el método Get del atributo 2
+    public int getOperando2(){
+        return this.operando2;
+    }
+
+    // Definimos el método Set del atributo 2
+    public void setOperando2(int operando2){
+        this.operando2 = operando2;
+    }
+
 }
--- src/prueba/pruebaAritmetica.java
+++ src/prueba/pruebaAritmetica.java
@@ -4,7 +4,10 @@ import aritmetica.Aritmetica;
 
 public class pruebaAritmetica {
     public static void main(String[] args) {
-        var aritmetica1 = new Aritmetica();
+        var aritmetica1 = new Aritmetica(5, 7);
+        System.out.println("Atributo operando1: " + aritmetica1.getOperando1());
+        aritmetica1.setOperando1(15);
+        aritmetica1.setOperando2(10);
         aritmetica1.sumar();
         aritmetica1.restar();
 
