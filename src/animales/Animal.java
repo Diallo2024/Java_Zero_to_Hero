26ffe5756a8d990635fd8397b3763695a81d4205
Diallo2024 <urymamadu@gmail.com>
2025-01-24 23:33:11 +0100

    Polimorfismo caso 1

--- src/animales/Animal.java
+++ src/animales/Animal.java
@@ -1,42 +1,37 @@
 package animales;
 
 public class Animal {
-    protected void comer(){
-        System.out.println("Como muchas veces al día");
-    }
-
-    protected void dormir(){
-        System.out.println("Duermo muchas horas al diá");
-    }
+   protected void hacerSonido(){
+       System.out.println("El animal hace un sonido");
+   }
 }
 
 class Perro extends Animal{
-    public void hacerSonido(){
-        System.out.println("Puedo ladrar");
-    }
-
     @Override
-    protected void  dormir(){
-        System.out.println("Duermo 15 horas al dí");
+    protected void hacerSonido(){
+        System.out.println("El perro hace wouf");
     }
 }
 
-class pruebaAnimal{
-    public static void main(String[] args) {
-        System.out.println("*** Ejemplo de Herencia ***");
-        System.out.println("Clase Padre, soy un animal");
-        var animal1 = new Animal();
-        animal1.comer();
-        animal1.dormir();
-        // animal1.hacerSonido(); // Este método no existe en la clase padre
+class Gato extends Animal{
+   @Override
+    protected void hacerSonido(){
+       System.out.println("El gato hace miau");
+   }
+}
 
-        System.out.println("\nClase Hija, soy un Perro");
-        var perro1 = new Perro();
-        perro1.comer();
-        perro1.dormir();
-        perro1.hacerSonido();
+class pruebaAnimal{
+
+        //Este es el método polimorfico
+   static void imprimirSonido(Animal animal){
+        animal.hacerSonido();
     }
 
+    public static void main(String[] args) {
+        var animal = new Animal();
+        imprimirSonido(animal);
+
+    }
 }
 
 
