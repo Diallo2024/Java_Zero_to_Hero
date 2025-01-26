499eb7e5a3236910688574d107d7adb9171eb0c2
Diallo2024 <urymamadu@gmail.com>
2025-01-26 01:33:15 +0100

    fixed: Orden class added to sistema de pagos app

---
+++ src/ventas/Orden.java
@@ -0,0 +1,21 @@
+package ventas;
+
+public class Orden {
+    private final int idOrden;
+    private Producto[] productos;
+    private int conadorProductos;
+    private static final int MAX_PRODUCTOS = 10;
+    private static int contadorOrdenes;
+
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+
+    public void agregarProducto(Producto producto){
+        if (this.conadorProductos < Orden.MAX_PRODUCTOS)
+        this.productos[this.conadorProductos++] = producto;
+        else
+            System.out.println("Se ha superado el máximo de productos: " + Orden.MAX_PRODUCTOS);
+    }
+}
