1662b0210f1ebccb1b91bc15675872ae1f0886d1
Diallo2024 <urymamadu@gmail.com>
2025-01-26 13:01:01 +0100

    Segundo casol uso de Parámetros variables

--- src/ArgumentosVariables.java
+++ src/ArgumentosVariables.java
@@ -1,9 +1,15 @@
 public class ArgumentosVariables {
     public static void main(String[] args) {
-        imprimirNumeros(1, 2, 3, 4, 5); // varargs
+       //imprimirNumeros(1, 2, 3, 4, 5); // varargs
+        variosParametro("María", 10, 20, 30);
     }
 
-     static void imprimirNumeros(int ... numeros) {
+    static void variosParametro(String nombre, int... numeros) {
+        System.out.println("Nombre: " + nombre);
+        imprimirNumeros(numeros);
+    }
+
+    static void imprimirNumeros(int ... numeros) {
         for (var i = 0; i < numeros.length; i++){
             System.out.print(numeros[i] + " ");
         }
