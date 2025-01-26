e6ed299e7dac53e6b392b50d2ce33b4d83545e28
Diallo2024 <urymamadu@gmail.com>
2025-01-26 13:16:23 +0100

    Compilando con ForEach

---
+++ src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int edades[] = {5, 10, 30, 40};
+        // Interando con el bucle Foreach
+        for (int edad : edades){
+            System.out.println("Edad: " + edad);
+        }
+    }
+}
