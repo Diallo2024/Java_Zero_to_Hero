77c136e0ff7cc662f2e9e2f2a12de5e6701da5ae
Diallo2024 <urymamadu@gmail.com>
2025-01-26 02:00:30 +0100

    Added first orden in sistema de pagos app

--- src/ventas/PruebaVentas.java
+++ src/ventas/PruebaVentas.java
@@ -4,9 +4,15 @@ public class PruebaVentas {
     public static void main(String[] args) {
         System.out.println("*** Sistema de Ventas ***");
         var producto1 = new Producto("Blusa", 30.00);
-        System.out.println(producto1);
+       // System.out.println(producto1);
 
         var producto2 = new Producto("Zapatos", 50.00);
-        System.out.println(producto2);
+       // System.out.println(producto2);
+
+        // Primer Orden
+        var orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOrden();
     }
 }
