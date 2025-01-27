451e0ccf262598222996fba7c9ca0035d0c31e32
Diallo2024 <urymamadu@gmail.com>
2025-01-27 13:22:52 +0100

    fixed: manejo de Excepciones con try/catch

---
+++ src/excepciones/Excepciones.java
@@ -0,0 +1,16 @@
+package excepciones;
+
+public class Excepciones {
+    // Hay dos tipos de excepciones: en tiempo compilación y de ejecución
+    public static void main(String[] args) {
+        int valor1 = 10, valor2 = 0;
+        try{
+            var resultado = valor1 / valor2;
+            System.out.println("resultado = " + resultado); // Esta operacion genera una excepción en tiempo de ejecución
+        }catch (Exception e){
+            System.out.println("Ocurrió un error: " + e);
+
+        }
+
+    }
+}
