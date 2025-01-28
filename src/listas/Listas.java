4e51702fb3d03e35a97a0d73c18144f5c58deb5c
Diallo2024 <urymamadu@gmail.com>
2025-01-28 13:25:39 +0100

    fixed: concepto de listas en java con funciones lampda

--- src/listas/Listas.java
+++ src/listas/Listas.java
@@ -1,11 +1,12 @@
 package listas;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Listas {
     public static void main(String[] args) {
-        List <String> milista = new ArrayList <>(); // Asi especificamos el tipo de dato que queremos almacenar en la lista
+        List <String> milista = new ArrayList <>(); // Asi especificamos el tipo de dato que queremos almacenar
         milista.add("Lunes");
         milista.add("Martes");
         milista.add("Miercoles");
@@ -15,13 +16,19 @@ public class Listas {
         milista.add("Sábado");
         milista.add("Domingo");
 
-        for (Object elemento: milista){
-            System.out.println("Día de la semana: " + elemento);
-        }
+       // for (Object elemento: milista){
+         //   System.out.println("Día de la semana: " + elemento);
+       // }
 
         // Funciones lambda (funcion anonima de un codgio compactp)
-        milista.forEach( elemento -> {
-            System.out.println("Elemento: " + elemento);
-        });
+       // milista.forEach( elemento -> {
+        //    System.out.println("Elemento: " + elemento);
+       // });
+
+        milista.forEach(System.out::println);
+
+        List<String> nombres = Arrays.asList("Juan", "Pedro", "Maria", "Elena");
+        System.out.println("\nLista de Nombres: ");
+        nombres.forEach(System.out::println);
     }
 }
